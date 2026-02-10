922bab178b3bcc131e0d5eae33c9f47025b5ab85
Jérémy Barbet <jeremgraph@gmail.com>
2026-02-10 15:55:44 +0100

    fix: tweak debug banner

--- src/webapp/features/analytics/mode/DebugModeBanner.tsx
+++ src/webapp/features/analytics/mode/DebugModeBanner.tsx
@@ -4,7 +4,7 @@ export function DebugModeBanner() {
   return (
     <div className="fixed top-12 z-20 lg:top-0 left-0 lg:left-64 right-0 mb-4">
       <div className="w-full border-t-4 border-t-warning text-center text-xs">
-        <div className="inline-block -mt-2 text-warning-foreground px-2 rounded-b-md bg-warning">
+        <div className="inline-block -mt-2 text-warning-foreground px-2 pb-1 rounded-b-md bg-warning">
           <TooltipProvider>
             <Tooltip delayDuration={0}>
               <TooltipTrigger>
