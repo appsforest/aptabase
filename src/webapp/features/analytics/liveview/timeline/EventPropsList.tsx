4dc4a1ebbed4fc2619e88e86be55f7abddb86789
Jérémy Barbet <jeremgraph@gmail.com>
2026-02-11 14:06:14 +0100

    fix: build issue

--- src/webapp/features/analytics/liveview/timeline/EventPropsList.tsx
+++ src/webapp/features/analytics/liveview/timeline/EventPropsList.tsx
@@ -7,10 +7,16 @@ export function EventPropsList(props: Props) {
   const stringProps = JSON.parse(props.stringProps ?? "{}");
   const numericProps = JSON.parse(props.numericProps ?? "{}");
 
-  const { locale, userId, proActive, ...eventProps } = {
+  const allProps = {
     ...stringProps,
     ...numericProps,
   }
+
+  const eventProps = Object.fromEntries(
+    Object.entries(allProps).filter(
+      ([key]) => !['locale', 'userId', 'proActive'].includes(key)
+    )
+  ) as Omit<typeof allProps, 'locale' | 'userId' | 'proActive'>;
 
   return (
     <div className="text-sm flex mt-1 ml-10 gap-2 flex-wrap">
